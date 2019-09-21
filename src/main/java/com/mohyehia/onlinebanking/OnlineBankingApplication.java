568b5c23b1da73bf08ed9a82712977268fb5c618
mohyehia <mohammedyehia99@gmail.com>
2019-09-22 00:04:41 +0200

    Initial commit

---
+++ src/main/java/com/mohyehia/onlinebanking/OnlineBankingApplication.java
@@ -0,0 +1,13 @@
+package com.mohyehia.onlinebanking;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnlineBankingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OnlineBankingApplication.class, args);
+	}
+
+}
