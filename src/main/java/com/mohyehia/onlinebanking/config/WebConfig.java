1cf78c28caa39fa2ef8f7e0b4df4ec4bf2f419bd
Mohamed.Yehia <Mohamed.Yehia@MohamedYehia.intercom.com.eg>
2019-09-24 23:25:49 +0200

    Add mvc config

---
+++ src/main/java/com/mohyehia/onlinebanking/config/WebConfig.java
@@ -0,0 +1,27 @@
+package com.mohyehia.onlinebanking.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.ViewResolver;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
+
+@Configuration
+@EnableWebMvc
+public class WebConfig implements WebMvcConfigurer {
+
+	@Bean
+	public ViewResolver jspViewResolver() {
+		InternalResourceViewResolver viewResolver = new InternalResourceViewResolver();
+		viewResolver.setPrefix("/WEB-INF/views/");
+		viewResolver.setSuffix(".jsp");
+		return viewResolver;
+	}
+
+	@Override
+	public void addResourceHandlers(ResourceHandlerRegistry registry) {
+		registry.addResourceHandler("/css/**").addResourceLocations("classpath:/static/css/");
+	}
+}
---
+++ src/main/java/com/mohyehia/onlinebanking/controllers/HelloController.java
@@ -0,0 +1,12 @@
+package com.mohyehia.onlinebanking.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HelloController {
+	@GetMapping("/hello")
+	public String hello() {
+		return "index";
+	}
+}
