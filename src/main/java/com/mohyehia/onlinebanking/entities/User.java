167a497522476d37b94e7cfb2e434da09e9fe828
mohyehia <mohammedyehia99@gmail.com>
2019-09-28 21:32:14 +0200

    Adding spring security and hibernate configuration

--- src/main/java/com/mohyehia/onlinebanking/entities/User.java
+++ src/main/java/com/mohyehia/onlinebanking/entities/User.java
@@ -1,6 +1,8 @@
 package com.mohyehia.onlinebanking.entities;
 
+import java.util.Collection;
 import java.util.Date;
+import java.util.HashSet;
 import java.util.Set;
 
 import javax.persistence.Entity;
@@ -12,8 +14,15 @@ import javax.persistence.JoinColumn;
 import javax.persistence.JoinTable;
 import javax.persistence.ManyToMany;
 
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
 @Entity
-public class User {
+public class User implements UserDetails {
+
+	private static final long serialVersionUID = 1L;
+
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
@@ -26,17 +35,21 @@ public class User {
 	private String password;
 	private boolean enabled;
 	private Date created;
-	
+	private boolean accountExpired;
+
+	private boolean credentialsExpired;
+
+	private boolean accountLocked;
+
 	@ManyToMany(fetch = FetchType.EAGER)
-	@JoinTable(name = "user_roles", 
-		joinColumns = { @JoinColumn(name = "user_id") },
-		inverseJoinColumns = { @JoinColumn(name = "role_id") })
+	@JoinTable(name = "user_roles", joinColumns = { @JoinColumn(name = "user_id") }, inverseJoinColumns = {
+			@JoinColumn(name = "role_id") })
 	private Set<Role> roles;
-	
-	public User(){
+
+	public User() {
 		this.created = new Date();
 	}
-	
+
 	public User(String username, String firstName, String lastName, String city, String address, String email, String password) {
 		this();
 		this.username = username;
@@ -51,64 +64,106 @@ public class User {
 	public Long getId() {
 		return id;
 	}
+
 	public void setId(Long id) {
 		this.id = id;
 	}
+
 	public String getUsername() {
 		return username;
 	}
+
 	public void setUsername(String username) {
 		this.username = username;
 	}
+
 	public String getFirstName() {
 		return firstName;
 	}
+
 	public void setFirstName(String firstName) {
 		this.firstName = firstName;
 	}
+
 	public String getLastName() {
 		return lastName;
 	}
+
 	public void setLastName(String lastName) {
 		this.lastName = lastName;
 	}
+
 	public String getCity() {
 		return city;
 	}
+
 	public void setCity(String city) {
 		this.city = city;
 	}
+
 	public String getAddress() {
 		return address;
 	}
+
 	public void setAddress(String address) {
 		this.address = address;
 	}
+
 	public String getEmail() {
 		return email;
 	}
+
 	public void setEmail(String email) {
 		this.email = email;
 	}
+
 	public String getPassword() {
 		return password;
 	}
+
 	public void setPassword(String password) {
 		this.password = password;
 	}
+
 	public boolean isEnabled() {
 		return enabled;
 	}
+
 	public void setEnabled(boolean enabled) {
 		this.enabled = enabled;
 	}
+
 	public Date getCreated() {
 		return created;
 	}
+
 	public Set<Role> getRoles() {
 		return roles;
 	}
+
 	public void setRoles(Set<Role> roles) {
 		this.roles = roles;
+	}
+
+	@Override
+	public Collection<? extends GrantedAuthority> getAuthorities() {
+		Set<GrantedAuthority> authorities = new HashSet<>();
+		roles.forEach(role -> authorities.add(new SimpleGrantedAuthority(role.getName())));
+		return authorities;
+	}
+
+	@Override
+	public boolean isAccountNonExpired() {
+		return !accountExpired;
+	}
+
+	@Override
+	public boolean isAccountNonLocked() {
+		return !accountLocked;
+	}
+
+	@Override
+	public boolean isCredentialsNonExpired() {
+		return !credentialsExpired;
 	}
 }
